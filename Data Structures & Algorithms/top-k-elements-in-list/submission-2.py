77ab51a63cdf0e5eac3991fa7be0c53dcb7c5590
Kayvon Pajouyan <166663424+KayvonPajouyan@users.noreply.github.com>
2026-05-27 06:09:18 -0800

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        numDict = {}
+        for num in nums:
+            numDict[num] = 1 if num not in numDict else numDict[num] + 1
+
+        #print(numDict)
+        res = {}
+        for i in range(k):
+            maxNumVal = -1001
+            maxNum = -100
+            for num in numDict:
+                #print(f"Adding in num: {num} with freq: {numDict[num]}")
+                if numDict[num] > maxNumVal and (num not in res):
+                    maxNumVal = numDict[num]
+                    maxNum = num
+                    #print(f"max num is {maxNum}")
+            res[maxNum] = maxNumVal
+    
+        return list(res.keys())
+        
+        
